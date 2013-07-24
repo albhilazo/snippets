3fd2bf4e74d66e66a458fdf2dceffa2c5058d6bd
Albert Hilazo <albert.hilazo@telefonica.net>
2013-07-24 08:58:38 +0000

    Added php/date - change_timezone

--- php/date.php
+++ php/date.php
@@ -2,7 +2,9 @@
 
 /**
  * Returns a given date in YYYYMMDDHHMMSS format
- * @param $row_date	Date in 'd/m/y H:i' format
+ * @link http://www.php.net/manual/en/datetime.createfromformat.php
+ * 
+ * @param $row_date		Date in 'd/m/y H:i' format
  */
 function get_date($row_date)
 {
@@ -11,6 +13,23 @@ function get_date($row_date)
 	$date = DateTime::createFromFormat('d/m/y H:i', $row_date);
 	// YYYYMMDDHHMMSS = YmdHis
 	return $date->format('YmdHis');
+}
+
+
+
+
+/**
+ * Returns converted date/time to another time zone
+ * @link http://www.epochconverter.com/programming/functions-php.php
+ * 
+ * @param $TimeStr			Original date/time
+ * @param $TimeZoneFrom		Original timezone
+ * @param $TimeZoneTo		Timezone wanted
+ */
+function change_timezone( $TimeStr="2012-01-01 12:00:00", $TimeZoneFrom="UTC", $TimeZoneTo="Europe/Andorra" )
+{
+	return date_create($TimeStr, new DateTimeZone($TimeZoneFrom))
+		->setTimezone(new DateTimeZone($TimeZoneTo))->format("Y-m-d H:i:s");
 }
 
 ?>
