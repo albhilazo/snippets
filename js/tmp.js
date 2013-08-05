137b370f229d13b2b7f98f100c99d4dc918a1092
Albert Hilazo <albert.hilazo@telefonica.net>
2013-08-05 10:53:06 +0000

    tmp.js edited online with Bitbucket

--- js/tmp.js
+++ js/tmp.js
@@ -1,4 +1,10 @@
 /* Go-top button */
+/* HTML
+<a id="goTop" class="img-circle">
+    <i class="top-icon icon-chevron-up icon-white"></i>
+</a>
+*/
+
 $(window).scroll(function() {
 	// Set offset inside scroll function in case it changes
 	// due to other dynamic elements
