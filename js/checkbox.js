1088949c879d726c9714b5781b918c073923d45d
albert <albhilazo@gmail.com>
2013-08-09 21:31:04 +0200

    tmp files classified

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,34 +1,6 @@
-/* Go-top button */
-/* HTML
-<a id="goTop" class="img-circle">
-    <i class="top-icon icon-chevron-up icon-white"></i>
-</a>
-*/
-
-$(window).scroll(function() {
-	// Set offset inside scroll function in case it changes
-	// due to other dynamic elements
-	var offset = $("#table-events").offset().top;
-
-    if ($(window).scrollTop() >= offset) {
-        $("#goTop").fadeIn();
-    } else {
-        $("#goTop").fadeOut();
-    }
-});
-
-
-
-
-$("#goTop").click(function() {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 400);
-});
-
-
 /* Check all checkboxes on click */
-/* HTML
+
+/** HTML **
 <label id="showall">Show:</label>
 <input type="checkbox" name="showfoo" value="checked" />
 <input type="checkbox" name="showbar" value="checked" />
---
+++ js/go-top.js
@@ -0,0 +1,28 @@
+/* Go-top button */
+
+/** HTML **
+<a id="goTop" class="img-circle">
+    <i class="top-icon icon-chevron-up icon-white"></i>
+</a>
+*/
+
+$(window).scroll(function() {
+	// Set offset inside scroll function in case it changes
+	// due to other dynamic elements
+	var offset = $("#table-events").offset().top;
+
+    if ($(window).scrollTop() >= offset) {
+        $("#goTop").fadeIn();
+    } else {
+        $("#goTop").fadeOut();
+    }
+});
+
+
+
+
+$("#goTop").click(function() {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+});
