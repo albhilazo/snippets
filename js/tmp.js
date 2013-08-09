09289d90d3415aa2d1a3aa517a83bccab03e2a54
Albert Hilazo <albert.hilazo@telefonica.net>
2013-08-09 10:49:32 +0000

    Check all checkboxes added - tmp.js edited online with Bitbucket

--- js/tmp.js
+++ js/tmp.js
@@ -25,3 +25,23 @@ $("#goTop").click(function() {
         scrollTop: 0
     }, 400);
 });
+
+
+/* Check all checkboxes on click */
+/* HTML
+<label id="showall">Show:</label>
+<input type="checkbox" name="showfoo" value="checked" />
+<input type="checkbox" name="showbar" value="checked" />
+<input type="checkbox" name="showbaz" value="checked" />
+*/
+
+$("#showall").click(function() {
+    var chk = $("input[name^='show']");
+    if ( chk.filter(":checked").length != chk.length ) {
+        // Check all if any unchecked
+        chk.prop('checked', true);
+    } else {
+        // Uncheck all if all checked
+        chk.prop('checked', false);
+    }
+});
