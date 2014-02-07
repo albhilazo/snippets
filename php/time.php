de0802898e470fc01bc2d94746afc0305bbc4fc8
Albert Hilazo <albhilazo@gmail.com>
2014-02-07 12:10:18 +0000

    Added memory usage test - time.php edited online with Bitbucket

--- php/time.php
+++ php/time.php
@@ -1,11 +1,32 @@
 <?php
+/**
+ * Get execution time for a piece of code
+ */
+    $time_start = microtime(true);
+    $mem_before = memory_get_usage(true);
+// CODE
+    $mem_after = memory_get_usage(true);
+    $time_end = microtime(true);
+
+    echo "<h4>Execution Time:      ". ($time_end - $time_start) ." seconds</h4>";
+    echo "<h4>Memory usage before: ". $mem_before ." bytes</h4>";
+    echo "<h4>Memory usage after:  ". $mem_after ." bytes</h4>";
+
+
+
 
 /**
- * Calculate execution time for a piece of code
+ * Get memory usage for a bigger section
  */
-$time_start = microtime(true); 
+    $cost = array();
+    $cost['start'] = memory_get_usage(true);
 // CODE
-$time_end = microtime(true);
-echo '<h4>Execution Time: '. ($time_end - $time_start) .' seconds</h4>';
+    $cost['check1'] = memory_get_usage(true);
+// MOAR CODE
+    $cost['check2'] = memory_get_usage(true);
+// EVEN MORE CODE
+    $cost['end'] = memory_get_usage(true);
+
+    echo "<pre><h4>Memory usage:</h4>"; var_dump($cost); echo "</pre>";
 
 ?>
