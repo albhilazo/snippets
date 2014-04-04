8b67f6f4130e41a11378b5f0db5c8d3846d5162c
Albert Hilazo <albhilazo@gmail.com>
2014-04-04 17:36:45 +0200

    Integrated go-top in animate.js

--- js/animate.js
+++ js/animate.js
@@ -1,4 +1,4 @@
-/* Animate to a specific point in the DOM */
+/* Animate to a specific point in the DOM *********************************************************/
 
 /** HTML **
 <ul class="nav nav-pills nav-stacked">
@@ -13,5 +13,58 @@ $('ul.nav>li>a').click(function(e) {
 
     $('body,html').animate({
         scrollTop: $($(e.target).attr('href')).offset().top
+    }, 400);
+});
+
+
+
+
+
+
+
+
+/* Go-top button **********************************************************************************/
+/** HTML **
+<a id="goTop" class="img-circle">
+    <i class="top-icon icon-chevron-up icon-white"></i>
+</a>
+*/
+
+/** CSS **
+#goTop {
+    display: none;
+    background: #addfee;
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+    z-index: 99;
+    }
+#goTop:hover { background: #2f96b4; }
+
+// For bootstrap glyphicons
+.top-icon {
+    width: 21px;
+    height: 21px;
+    margin: 6px;
+    background-size: 703.5px 238.5px;
+    background-position: -432px -181px;
+    }
+*/
+
+$(window).scroll(function() {
+    if($('.goTop-point').length) {
+        var offset = $('.goTop-point').offset().top;
+
+        if ($(window).scrollTop() >= offset) {
+            $('#goTop').fadeIn();
+        } else {
+            $('#goTop').fadeOut();
+        }
+    }
+});
+
+$("#goTop").click(function() {
+    $('body,html').animate({
+        scrollTop: 0
     }, 400);
 });
